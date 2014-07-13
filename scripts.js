8d1100d70a13e53b95523c59a8582d5bb56f1fed
Alex Sørlie Glomsaas <alex@delexious.com>
2014-07-13 23:22:32 +0200

    put nick colouring code in a class

--- scripts.js
+++ scripts.js
@@ -7,6 +7,79 @@
 var overrideActions = true;
 var mappedSelectedUsers = new Array();
 
+var NickColorGenerator = (function () {
+    function NickColorGenerator(message) {
+        var messageContainer = message.children[0].children[1];
+        //Start alternative nick colouring procedure
+        var selectNick = messageContainer.children[0];
+        selectNick.removeAttribute('colornumber');
+        var nickcolor = this.generateColorFromHash(selectNick.getAttribute('nickname'));
+        selectNick.style.color = nickcolor;
+        var inlineNicks = messageContainer.querySelectorAll('.inline_nickname');
+        if (message.getAttribute('ltype') == 'action' && overrideActions) {
+            selectNick.children[0].style.color = nickcolor;
+            messageContainer.style.color = nickcolor;
+        }
+        for (var i = 0, len = inlineNicks.length; i < len; i++) {
+            inlineNicks[i].removeAttribute('colornumber');
+            var nick = inlineNicks[i].innerHTML;
+            if (inlineNicks[i].getAttribute('mode').length > 0) {
+                nick = nick.replace(inlineNicks[i].getAttribute('mode'), '');
+            }
+            inlineNicks[i].style.color = this.generateColorFromHash(nick);
+        }
+    }
+    NickColorGenerator.prototype.sanitiseNickname = function (nick) {
+        // attempts to clean up a nickname
+        // by removing alternate characters from the end
+        // nc_ becomes nc, avidal` becomes avidal
+        nick = nick.toLowerCase();
+        // typically ` and _ are used on the end alone
+        nick = nick.replace(/[`_]+$/, '');
+        // remove |<anything> from the end
+        nick = nick.replace(/|.*$/, '');
+        return nick;
+    };
+
+    NickColorGenerator.prototype.generateHashFromNickname = function (nick) {
+        var cleaned = this.sanitiseNickname(nick);
+        var h = 0;
+        for(var i = 0; i < cleaned.length; i++) {
+            h = cleaned.charCodeAt(i) + (h << 6) + (h << 16) - h;
+        }
+        return h;
+    };
+
+    NickColorGenerator.prototype.generateColorFromHash = function (nick) {
+        var nickhash = this.generateHashFromNickname(nick);
+        var deg = nickhash % 360;
+        var h = deg < 0 ? 360 + deg : deg;
+        var l = Math.abs(nickhash) % 110;
+        if(h >= 30 && h <= 210) {
+            l = 40;
+        }
+        var s = 20 + Math.abs(nickhash) % 80;
+        if (h >= 210 && s >= 80) {
+            s = s-30;
+        }
+        if ((h < 110 && s < 60) || l <= 30) {
+            l = l + 30;
+        }
+        if (l > 80) {
+            l = l - 20;
+        }
+        return "hsl(" + h + "," + s + "%," + l + "%)";
+    };
+    return NickColorGenerator;
+})();
+
+
+var GeneralEventManager = (function () {
+    function GeneralEventManager() {
+    }
+})();
+
+
 Textual.viewFinishedLoading = function() {
     Textual.fadeInLoadingScreen(1.00, 0.95);
 
@@ -21,36 +94,19 @@ Textual.viewFinishedReload = function() {
 
 Textual.newMessagePostedToView = function (line) {
     var message = document.getElementById('line-' + line);
-	var messageContainer = message.children[0].children[1];
 	if (message.getAttribute('ltype') == 'privmsg' || message.getAttribute('ltype') == 'action') {
-        //Start alternative nick colouring procedure
-        var selectNick = messageContainer.children[0];
-        selectNick.removeAttribute('colornumber');
-        var nickcolor = get_color(selectNick.getAttribute('nickname'));
-        selectNick.style.color = nickcolor;
-        var inlineNicks = messageContainer.querySelectorAll('.inline_nickname');
-        if (message.getAttribute('ltype') == 'action' && overrideActions) {
-            selectNick.children[0].style.color = nickcolor;
-            messageContainer.style.color = nickcolor;
-        }
-        for (var i = 0, len = inlineNicks.length; i < len; i++) {
-            inlineNicks[i].removeAttribute('colornumber');
-            var nick = inlineNicks[i].innerHTML;
-            if (inlineNicks[i].getAttribute('mode').length > 0) {
-                nick = nick.replace(inlineNicks[i].getAttribute('mode'), '');
-            }
-            inlineNicks[i].style.color = get_color(nick);
-        }
+        new NickColorGenerator(message);
     }
-    var element = document.getElementById("line-" + line);
 
-    updateNicknameAssociatedWithNewMessage(element);
+    updateNicknameAssociatedWithNewMessage(message);
 };
 
 Textual.nicknameSingleClicked = function(e)
 {
     userNicknameSingleClickEvent(e);
 }
+
+
 
 function updateNicknameAssociatedWithNewMessage(e)
 {
@@ -108,49 +164,4 @@ function userNicknameSingleClickEvent(e)
             }
         }
     }
-}
-
-
-/* Based on irccloud-colornicks by Alex Vidal. See LICENSE for copyright.  */
-
-function clean_nick(nick) {
-    // attempts to clean up a nickname
-    // by removing alternate characters from the end
-    // nc_ becomes nc, avidal` becomes avidal
-    nick = nick.toLowerCase();
-    // typically ` and _ are used on the end alone
-    nick = nick.replace(/[`_]+$/, '');
-    // remove |<anything> from the end
-    nick = nick.replace(/|.*$/, '');
-    return nick;
-}
-
-function hash(nick) {
-    var cleaned = clean_nick(nick);
-    var h = 0;
-    for(var i = 0; i < cleaned.length; i++) {
-        h = cleaned.charCodeAt(i) + (h << 6) + (h << 16) - h;
-    }
-    return h;
-}
-
-function get_color(nick) {
-    var nickhash = hash(nick);
-    var deg = nickhash % 360;
-    var h = deg < 0 ? 360 + deg : deg;
-    var l = Math.abs(nickhash) % 110;
-    if(h >= 30 && h <= 210) {
-        l = 40;
-    }
-    var s = 20 + Math.abs(nickhash) % 80;
-    if (h >= 210 && s >= 80) {
-        s = s-30;
-    }
-    if ((h < 110 && s < 60) || l <= 30) {
-        l = l + 30;
-    }
-    if (l > 80) {
-        l = l - 20;
-    }
-    return "hsl(" + h + "," + s + "%," + l + "%)";
 }
