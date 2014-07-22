691002cb843ba0a048a52d935966ab947790fc7b
Alex Sørlie Glomsaas <alex@delexious.com>
2014-07-22 22:50:52 +0200

    Support notices for nick colouring

--- scripts.js
+++ scripts.js
@@ -102,7 +102,8 @@ Textual.nicknameSingleClicked = function(e) {
 function updateNicknameAssociatedWithNewMessage(e) {
 	/* We only want to target plain text messages. */
 	var elementType = e.getAttribute("ltype");
-	if (elementType == "privmsg" || elementType == "action") {
+    var acceptedElementTypes = ["privmsg", "action", "notice"];
+	if (acceptedElementTypes.indexOf(elementType) !== -1) {
 		/* Get the nickname information. */
 		var senderSelector = e.querySelector(".sender");
 		if (senderSelector) {
